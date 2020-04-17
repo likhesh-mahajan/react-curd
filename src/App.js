65a7368b96a5c1ecb8e37c55cd27d598b6dc37b5
Likhesh Mahajan <likhesh@Mac-Pro.local>
2020-04-17 12:23:06 +1000

    CSS classname corrected

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
   };
 
   return (
-    <div className="conatiner">
+    <div className="container">
       <h1>CURD App with hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
